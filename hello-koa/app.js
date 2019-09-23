0c767996fa5096362a38544565d9f7dc103facb5
pzhang <pzhang@whtb.tech>
2019-09-23 15:28:41 +0800

    koa

--- hello-koa/app.js
+++ hello-koa/app.js
@@ -1,69 +1,43 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-// 创建一个Koa对象表示web app本身:
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
+const templating = require('./templating');
+
 const app = new Koa();
 
-// 为什么要调用await next()？
-// 原因是koa把很多async函数组成一个处理链，每个async函数都可以做一些自己的事情，然后用await next()来调用下一个async函数。我们把每个async函数称为middleware，这些middleware可以组合起来，完成很多有用的功能。
-app.use(async (ctx, next) => {
-    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-    await next(); // 调用下一个middleware
-});
+const isProduction = process.env.NODE_ENV === 'production';
 
+// log request URL:
 app.use(async (ctx, next) => {
-    const start = new Date().getTime(); // 当前时间
-    await next(); // 调用下一个middleware
-    const ms = new Date().getTime() - start; // 耗费时间
-    console.log(`Time: ${ms}ms`); // 打印耗费时间
-});
-
-// 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-    // 参数ctx是由koa传入的封装了request和response的变量，可以通过它访问request和response，next是koa传入的将要处理的下一个异步函数。
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+        start = new Date().getTime(),
+        execTime;
     await next();
-    // 设置response的Content-Type:
-    ctx.response.type = 'text/html';
-     // 设置response的内容:
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
-app.use(async (ctx, next) => {
-    if (ctx.request.path === '/') {
-        ctx.response.body = 'index page';
-    } else {
-        await next();
-    }
-});
+// static file support:
+if (! isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
 
-app.use(async (ctx, next) => {
-    if (ctx.request.path === '/test') {
-        ctx.response.body = 'TEST page';
-    } else {
-        await next();
-    }
-});
+// parse request body:
+app.use(bodyParser());
 
-app.use(async (ctx, next) => {
-    if (ctx.request.path === '/error') {
-        ctx.response.body = 'ERROR page';
-    } else {
-        await next();
-    }
-});
+// add nunjucks as view:
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
 
-// 调用app.use()的顺序决定了middleware的顺序
-// middleware没有调用await next()，后续的middleware将不再执行了
-app.use(async (ctx, next) => {
-    if (await checkUserPermission(ctx)) {
-        await next();
-    } else {
-        ctx.response.status = 403;
-    }
-});
+// add controller:
+app.use(controller('.'));
 
-// 在端口3000监听:
 app.listen(3000);
 console.log('app started at port 3000...');
-
-// 启动程序的命令：node app.js 或者 npm start
